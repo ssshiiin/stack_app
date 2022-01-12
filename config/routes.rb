2ab0eed8553dc54dfa09b16a9d1f696502cf4e9e
Takumi SHINGAI <s1f101902433@iniad.org>
2022-01-12 20:22:40 +0900

    delete add

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/users/index' => "users#index"
   get '/users/:id' => "users#show"
   post '/users/:id' => "posts#save"
+  delete '/posts/:id' => "posts#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
