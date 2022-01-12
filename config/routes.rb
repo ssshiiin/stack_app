2208f635336636901fcb80354065939f86fd220e
Takumi SHINGAI <s1f101902433@iniad.org>
2022-01-12 17:24:17 +0900

    app design

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   post '/login/:id' => "users#login"
   get '/users/index' => "users#index"
   get '/users/:id' => "users#show"
-  post '/posts/save/' => "posts#save"
+  post '/users/:id' => "posts#save"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
